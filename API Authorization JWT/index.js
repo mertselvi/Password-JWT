a4f24c659c08c25fde357f3cc3ac2e080d837308
M.Mert Selvi <145585311+mertselvi@users.noreply.github.com>
2023-11-28 00:12:01 +0300

    Add files via upload

---
+++ API Authorization JWT/index.js
@@ -0,0 +1,54 @@
+const expres = require('express')
+const jwt = require('jsonwebtoken')
+
+const app = expres();
+const user = {
+  id : 1,
+  user : "Mert Selvi",
+  contact : {
+    email : "mert@gmail.com",
+    tel : "534576605345"
+  }
+}
+
+
+app.post("/verify", verifyToken, (req,res)=> {
+  jwt.verify(req.token, 'secretKey', (message,AuthData) => {
+    message = req.message;
+    res.send({message, AuthData});
+  })
+
+})
+
+app.post("/login", (req,res)=> {
+  jwt.sign(user,'secretKey', {expiresIn : "10s"}, (message,token) => {
+    res.send ({message : "Token created succesfully", token : token})
+  });
+  console.log(tokenD);
+})
+
+function verifyToken (req,res,next) {
+  const bearerHeader = req.headers['authorization'];
+
+  if( typeof(bearerHeader) !== 'undefined') {
+
+    // Authorization başındaki bearer kısmını çıkartıp bir de öyle istek at.
+    const bearerArray = bearerHeader.split(" ");
+    req.token = bearerArray[1];
+    req.message = "Token created succesfully";
+    next();
+  } else {
+    res.status(403).send({message : "Authorization Fail"}) //message kısmını düzenle
+  }
+
+}
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server listen on 3000');
+})
+
